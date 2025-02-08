066380999b701710302fb1d182c4ef3e989441a5
y7x8f9123 <156929552+y7x8f9123@users.noreply.github.com>
2025-02-08 16:26:32 -0500

    Handles user authentication.

--- API/bp_auth.py
+++ API/bp_auth.py
@@ -1,6 +1,9 @@
 
 # auth.py
-from flask import Blueprint, jsonify, request, make_response
+from flask import Blueprint, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from datetime import timedelta
+
 from request_utils import resp_json
 from request_utils import check_form
 from account_db import find_account_by_name, find_account_by_email, save_account, check_account
@@ -20,10 +23,10 @@ def register():
     acc = params['data']
     old = find_account_by_name(acc['name'])
     if old is not None:
-        return resp_json(1, 'User name alread exists.')
+        return resp_json(1, 'User name alread exists.'), 400
     old = find_account_by_email(acc['email'])
     if old is not None:
-        return resp_json(1, 'User email alread exists.')
+        return resp_json(1, 'User email alread exists.'), 400
     
     save_account(acc['name'], acc['email'], acc['password'])
     account = find_account_by_email(acc['email'])
@@ -37,6 +40,11 @@ def login():
     acc = params['data']
     checked = check_account(acc['email'], acc['password'])
     if checked is None:
-        return resp_json(1, 'Email or password wrong.')
+        return resp_json(1, 'Email or password wrong.'), 401
     else:
-        return resp_json(0, checked)
+        # default expires delta is 15 minutes
+        # access_token = create_access_token(identity=checked['id'])
+        access_token = create_access_token(identity='user_id', expires_delta=timedelta(days=1))  # set expires 1 day
+        # print('token is', access_token)
+        # return jsonify({'access_token': access_token}), 200
+        return resp_json(0, {'user':checked, 'token':access_token})
