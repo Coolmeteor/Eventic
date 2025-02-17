df3366df040dadfba0a3e4ed037ce819b3617872
YILIPERCY <liyipercy@gmail.com>
2025-02-16 23:06:33 -0500

    Add files via upload

--- eventic-fe/src/pages/login.tsx
+++ eventic-fe/src/pages/login.tsx
@@ -1,8 +1,9 @@
 import DefaultLinkButton from "@/components/DefaultLinkButton";
 import Section from "@/components/Section";
 import { useState, useEffect } from "react";
-import { API } from "./_app";
+import {API} from "./_app";
 import { data } from "autoprefixer";
+
 
 export default function Login() {
 
@@ -17,214 +18,176 @@ export default function Login() {
     const [errorText, setErrorText] = useState<string>("");
 
     async function login(email: string, password: string) {
-        if (email == undefined || email == "") {
-            setErrorText("Please enter your username");
-            return
+        if (!email) {
+            setErrorText("Please enter your email");
+            return;
         }
-        if (password == undefined || password == "") {
+        if (!password) {
             setErrorText("Please enter your password");
-            return
+            return;
         }
         setErrorText("");
-        setEmail(email);
-        setPassword(password);
-
-
-
-        const data = {
-        
-            password: password,
-            email: email,
-        }
-
-
+    
+        const data = { email, password };
+    
         try {
             const response = await fetch(`${API}/auth/login`, {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data),
-                 mode: "no-cors"
             });
-
+    
             if (!response.ok) {
-                throw new Error(`Failed to upload data to server: ${response.statusText}`);
+                throw new Error(`Login failed: ${response.statusText}`);
             }
-
-            return await response.json();
+    
+            const result = await response.json();
+            console.log("Login Success:", result);
+            if (response.ok) {
+                alert("Login successful!");
+                setIsLogin(true);
+            } else {
+                setErrorText(result.msg || "Invalid credentials");
+            }
+    
         } catch (error) {
-            setErrorText((error as Error).message);
-            console.error("Error posting event:", error);
-            
-            return null;
+            setErrorText("Login error: " + (error as Error).message);
+            console.error("Login error:", error);
         }
-
-
-       
     }
-
     async function register(email: string, username: string, password: string, confirmPassword: string) {
-
-        if (username == undefined || username == "") {
+        if (!username) {
             setErrorText("Please enter your username");
-            return
+            return;
         }
-        if (password == undefined || password == "") {
+        if (!password) {
             setErrorText("Please enter your password");
-            return
+            return;
         }
-        // if (email == undefined || email == "") {
-        //     setErrorText("Please enter your email");
-        //     return
-        // }
         if (password.length < 5) {
             setErrorText("Password must be at least 5 characters long");
-            return
+            return;
         }
-        if (password != confirmPassword) {
+        if (password !== confirmPassword) {
             setErrorText("Passwords do not match");
-            return
+            return;
         }
         setErrorText("");
-        setUsername(username);
-        setPassword(password);
-        setConfirmPassword(confirmPassword);
-        setEmail(email);
-
-
+    
         const data = {
-            name: username,
-            email: email,
-            password: password,
-        }
-
-        console.log(data);
+            user_name: username,  // ✅ 确保参数匹配后端
+            email,
+            password,
+            
+        };
+    
         try {
             const response = await fetch(`${API}/auth/register`, {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data),
-                 mode: "no-cors"
             });
-
+    
             if (!response.ok) {
-                throw new Error(`Failed to upload data to server: ${response.statusText}`);
+                throw new Error(`Registration Failed: ${response.statusText}`);
             }
-
-            return await response.json();
+    
+            const result = await response.json();
+            console.log("Registration Success:", result);
+            if (response.ok) {
+                alert("Registration successful! Please log in.");
+                setIsLogin(true);
+            } else {
+                setErrorText(result.msg || "Registration failed");
+            }
+    
         } catch (error) {
-            setErrorText((error as Error).message);
-            console.error("Error posting event:", error);
-            
-            return null;
+            setErrorText("Registration error: " + (error as Error).message);
+            console.error("Registration error:", error);
         }
-
     }
-
-    // useEffect(() => {
-    //     console.log(username, password, email)
-    // }, [username, password, email]);
+    
+    
+   
 
     return (
-        <>
-            <Section>
-                {
-                    // switch between register and login
-                    isLogin ?
-                        (
-                            <div className="login">
-                                <h1>Login</h1>
-                                <form onSubmit={(e) => {
-                                    e.preventDefault();
-                                    login(e.target[0].value, e.target[1].value);
-                                }}>
-                                    <input id="email" type="email" placeholder="Email address" required={true}/>
-                                    <input id="password" type="password" placeholder="Password" required={true} />
-                                    {errorText && <p className="errortext">{errorText}</p>}
-                                    <button type="submit">Login</button>
-                                </form>
-
-                                <p>Don't have an account? <DefaultLinkButton onClick={() => setIsLogin(false)}>Create one</DefaultLinkButton></p>
-                            </div>
-                        )
-                        :
-                        (
-                            <div className="login">
-                                <h1>Create an account</h1>
-                                <form onSubmit={(e) => {
-                                    e.preventDefault();
-                                    register(e.target[3].value, e.target[0].value, e.target[1].value, e.target[2].value);
-                                }
-                                }>
-                                    <input id="email" type="email" placeholder="Email address" required={true}/>
-                                    <input id="username" type="text" placeholder="Username" required={true} />
-                                    <input id="password" type="password" placeholder="Password" required={true} />
-                                    <input id="passwordconf" type="password" placeholder="Confirm Password" required={true} />
-
-                                    {errorText && <p className="errortext">{errorText}</p>}
-                                    <button type="submit">Register</button>
-                                </form>
-
-                                <p>Already have an account? <DefaultLinkButton onClick={() => setIsLogin(true)}>Login</DefaultLinkButton></p>
-                            </div>
-                        )
-                }
-
-
-            </Section>
+        <Section>
+            {isLogin ? (
+                <div className="login">
+                    <h1>Login</h1>
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        login(e.currentTarget.email.value, e.currentTarget.password.value);
+                    }}>
+                        <input id="email" type="email" placeholder="Email address" required />
+                        <input id="password" type="password" placeholder="Password" required />
+                        {errorText && <p className="errortext">{errorText}</p>}
+                        <button type="submit">Login</button>
+                    </form>
+                    <p>Don't have an account? <DefaultLinkButton onClick={() => setIsLogin(false)}>Create one</DefaultLinkButton></p>
+                </div>
+            ) : (
+                <div className="login">
+                    <h1>Create an account</h1>
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        register(
+                            e.currentTarget.email.value,
+                            e.currentTarget.username.value,
+                            e.currentTarget.password.value,
+                            e.currentTarget.passwordconf.value
+                        );
+                    }}>
+                        <input id="email" type="email" placeholder="Email address" required />
+                        <input id="username" type="text" placeholder="Username" required />
+                        <input id="password" type="password" placeholder="Password" required />
+                        <input id="passwordconf" type="password" placeholder="Confirm Password" required />
+                        {errorText && <p className="errortext">{errorText}</p>}
+                        <button type="submit">Register</button>
+                    </form>
+                    <p>Already have an account? <DefaultLinkButton onClick={() => setIsLogin(true)}>Login</DefaultLinkButton></p>
+                </div>
+            )}
             <style jsx>{`
-
-            .login {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                justify-content: center;
-                padding: 1rem;
-            }
-
-            .login h1 {
-                font-size: 2rem;
-                margin-bottom: 1rem;
-            }
-
-            .login form {
-
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                justify-content: center;
-                padding: 1rem;
-            }
-
-            .login input {
-                padding: 0.5rem;
-                margin: 0.5rem;
-                width: 20rem;
-                background-color: var(--color-background-mid);
-            }
-
-            .login button {
-                padding: 0.5rem;
-                margin: 0.5rem;
-                width: 20rem;
-                background-color: var(--color-primary);
-                color: black;
-                border: none;
-            }   
-
-            .login button:hover {
-                background-color: var(--color-primary-dark);
-            }
-
-            .errortext {
-                color: red;
-            }
-
+                .login {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                    padding: 1rem;
+                }
+                .login h1 {
+                    font-size: 2rem;
+                    margin-bottom: 1rem;
+                }
+                .login form {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                    padding: 1rem;
+                }
+                .login input {
+                    padding: 0.5rem;
+                    margin: 0.5rem;
+                    width: 20rem;
+                    background-color: var(--color-background-mid);
+                }
+                .login button {
+                    padding: 0.5rem;
+                    margin: 0.5rem;
+                    width: 20rem;
+                    background-color: var(--color-primary);
+                    color: black;
+                    border: none;
+                }
+                .login button:hover {
+                    background-color: var(--color-primary-dark);
+                }
+                .errortext {
+                    color: red;
+                }
             `}</style>
-        </>
-
+        </Section>
     );
 }
