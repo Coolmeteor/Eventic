266d3710c08d67d3ce1eb1ed41c2a94ccccde12f
ShoeiBrock <sy21uu@brocku.ca>
2025-02-21 01:32:22 -0500

    Move api to the right folder

--- API/readme.md
+++
@@ -1,4 +0,0 @@
-# start server
-> flask run
-
-access api from browser, http://127.0.0.1:5000
--- eventic-fe/src/components/Layouts/ProfileLayout.tsx
+++ eventic-fe/src/components/Layouts/ProfileLayout.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, ReactNode } from "react";
+import { useState, ReactNode } from "react";
 import Split from "react-split";
 import { useRouter } from "next/router";
 import DashboardList from "@/components/Profile/DashboardList";
