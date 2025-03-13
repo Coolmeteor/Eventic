a0da3c916986ac424595ede514e7bd1de58827eb
mikooomich <mikooomichael@gmail.com>
2025-03-13 18:20:57 -0400

    wip checkout page

--- eventic-fe/src/components/EventCard.tsx
+++ eventic-fe/src/components/EventCard.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendar, faLocationArrow, faPaperclip, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { faCalendar, faLocationArrow, faSquarePersonConfined, } from '@fortawesome/free-solid-svg-icons'
 import { DEV_MODE, EventData } from '@/constants'
 import DefaultButton from './DefaultButton'
 
 type Props = {
     large: boolean,
     event: EventData,
-    btn?:{
+    btn?: {
         click: () => void | undefined,
         text?: string | undefined,
     }
@@ -16,7 +16,7 @@ type Props = {
 
 const borderRadius = "15px";
 
-export default function EventCard({ large = false, event, btn }: Props) {
+export function EventCard({ large = false, event, btn }: Props) {
     return (
         <>
             <div className="eventContainer">
@@ -55,7 +55,7 @@ export default function EventCard({ large = false, event, btn }: Props) {
 
                     </div>
                     {btn &&
-                        <DefaultButton textColor='var(--color-onPrimary)' bgColor={['000000', '000000','000000']} onClick={btn.click}>{btn.text}</DefaultButton>
+                        <DefaultButton textColor='var(--color-onPrimary)' bgColor={['000000', '000000', '000000']} onClick={btn.click}>{btn.text}</DefaultButton>
 
                     }
                 </div>
@@ -244,6 +244,248 @@ export default function EventCard({ large = false, event, btn }: Props) {
                     width: 100%;
 
                     height: 70%;
+                    background: linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.61) 50%, rgba(0, 0, 0, 0.8) 100%);
+                    
+                    // height: 60%;
+                    // background: linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.8) 100%);
+                    border-radius: ${borderRadius} ${borderRadius} 0px 0px;
+                }
+                `}
+            </style>
+        </>
+    )
+}
+
+
+
+export function EventCardLarge({ event }: Props) {
+    return (
+        <>
+            <div className="eventContainer">
+                <div className='left'>
+                    {/* event image */}
+                    <div className="image-wrapper">
+                        <img src={event.media[0]} alt={event.name} className="event-image" />
+                        <div className="image-overlay"></div>
+                    </div>
+
+                    {/* event title and location */}
+                    <div className='top'>
+
+                        <div className="location-text">
+                            <FontAwesomeIcon icon={faLocationArrow} />
+                            <p>{event.location_string}</p>
+                        </div>
+                        <h3>{event.name}</h3>
+                    </div>
+                </div>
+
+
+                {/* decription and other detail */}
+                <div className='event-content'>
+
+                    <div className='organizer'>
+                        <FontAwesomeIcon icon={faSquarePersonConfined} fontSize={"70px"} />
+                        <p>{event.creator}</p>
+                    </div>
+                    <div className="date-text">
+                        <FontAwesomeIcon icon={faCalendar} />
+                        <p>{new Date(event.start_date).toLocaleDateString()} — {new Date(event.start_date).toLocaleDateString()}</p>
+                    </div>
+
+                    <p className='event-desc'>{event.description}</p>
+                </div>
+            </div>
+
+
+            <style jsx>
+                {`
+                .icon {
+                    {/* width: 60px;
+                    height: 40px; */}
+
+                    align-self: start;
+                    position: relative;
+
+                    margin: 20px 20px 0px 0px;
+                    font-size: 30px;
+                }
+
+                .eventContainer {
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+                    justify-content: space-between;
+
+                    min-width: 250px;
+                    max-width: 1000px;
+                    height: 250px;
+                  
+                    box-shadow: var(--shadow-large);
+                    border-radius: ${borderRadius};
+        
+                    font-size: var(--font-size-body-S);
+                    font-family: var(--font-roboto);
+                }
+
+                .event-content {
+                    display: flex;
+                    flex-direction: column;
+                    padding: 0px 20px 10px 20px;
+
+                    width: 100%;
+                    height: 100%;
+                    border-radius: ${borderRadius};
+                    background-color: var(--color-background-mid);
+                }
+
+                h3 {
+                    font-size: var(--font-size-header-XS);
+                
+                    align-self: start;
+                    max-width: 300px;
+                
+
+                    // all this is for max 2 lines
+                    display: -webkit-box;
+                    -webkit-line-clamp: 3;
+                    -webkit-box-orient: vertical;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                }
+
+                .event-desc {
+                    // all this is for max 8 lines
+                    display: -webkit-box;
+                    -webkit-line-clamp: 6;
+                    -webkit-box-orient: vertical;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                }
+
+                p {
+                    width: 100%;
+                }
+
+
+                span {
+                    display: flex;
+                    margin-top: 40px;
+                    width: 100%;
+                }
+
+                .left {
+
+                    display: flex;
+                    flex-direction: column;
+                    
+                    width: 100%;
+                    max-width: 230px;
+                    height: 100%;
+                    border-radius: ${borderRadius};
+                }
+                .top {
+                    display: flex;
+                    flex-direction: column;
+
+                    width: 100%;
+                    margin-bottom: ${borderRadius};
+                    margin-top: 10px;
+                    padding-right: 20px;
+                    padding-left: 30px;
+
+                    color: white;
+
+                    border-radius: ${borderRadius} ${borderRadius} 0px 0px;
+                }
+
+
+                .organizer {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: left;
+                    align-items: center;
+                    padding: 0.5em 0.2em 0.5em 0em;
+                    gap: 1em;
+                    font-size: 1.6em;
+                }
+
+                
+                .location-text {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: end;
+                    padding: 1em 0em 1em 0.5em;
+
+                    align-self: end;
+                }
+                .location-text p {
+                    margin-left: 10px;
+
+              
+                }
+
+                .date-text {
+                    display: flex;
+                    flex-direction: row;
+                
+                    padding: 1em 1em 1em 0.5em;
+
+                    align-self: start;
+                }
+                .date-text p {
+                    margin-left: 10px;
+                }
+
+
+                .location-date {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: left;
+                    align-items: top;
+                    background-color: rgb(189, 224, 214);
+                    padding: 1em;
+                }
+
+
+
+                .location-date-inner {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: left;
+                    align-items: top;
+            
+                    padding: 1em;
+                    margin-right: 1em;
+                }
+
+                .location-date-inner * {
+                    margin-left: 1em;
+                }
+
+
+
+                .image-wrapper {
+                    position: absolute;
+                    width: 250px;
+                    height: 250px;
+                    overflow: hidden;
+                    z-index: -9999999;
+                }
+
+                .event-image {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    border-radius: ${borderRadius} 0px 0px ${borderRadius};
+                }
+
+                .image-overlay {
+                    position: absolute;
+                    top: 0;
+                    width: 100%;
+
+                    height: 100%;
                     background: linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.61) 50%, rgba(0, 0, 0, 0.8) 100%);
                     
                     // height: 60%;
